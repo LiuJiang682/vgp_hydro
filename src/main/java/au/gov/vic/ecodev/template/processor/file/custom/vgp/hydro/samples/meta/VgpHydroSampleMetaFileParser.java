453f65d8fd5751fe4caecce75080a0791f086991
vicey3n <vicey3n@192.168.56.1>
2018-07-26 13:37:54 +1000

    Added VgpHydroSamplesMetaProcessor class.

---
+++ src/main/java/au/gov/vic/ecodev/template/processor/file/custom/vgp/hydro/samples/meta/VgpHydroSampleMetaFileParser.java
@@ -0,0 +1,18 @@
+package au.gov.vic.ecodev.template.processor.file.custom.vgp.hydro.samples.meta;
+
+import java.io.File;
+
+import au.gov.vic.ecodev.mrt.template.processor.context.TemplateProcessorContext;
+
+public class VgpHydroSampleMetaFileParser {
+
+	public VgpHydroSampleMetaFileParser(File file, TemplateProcessorContext context) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void parse() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
